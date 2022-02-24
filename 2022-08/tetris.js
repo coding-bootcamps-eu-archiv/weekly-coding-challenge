4f907fbbd40e2619174702b09ccbeb8df2e15e7d
Nico Koenig <nico.koenig@coding-bootcamps.eu>
2022-02-24 19:05:58 +0100

    feat: adds final minimal tetris game

--- 2022-08/tetris.js
+++ 2022-08/tetris.js
@@ -4,6 +4,8 @@ let width = 200;
 let blockSize = 20;
 let blocks = [];
 let colors = ["hotpink", "lemonchiffon", "salmon"];
+let gameover = false;
+let points = 0;
 
 function setup() {
   createCanvas(width, height);
@@ -16,22 +18,65 @@ function setup() {
 
 function draw() {
   background(0);
+
+  text("Score: " + points, 140, 20);
+
   fill(block.c);
   rect(block.x, block.y, blockSize, blockSize);
-  if (block.y < height - blockSize) {
+
+  const maxY = getMaxYforX(block.x);
+
+  if (maxY === 0) {
+    background("black");
+    fill("red");
+    textSize(20);
+    text("Game Over", 50, 50);
+    gameover = true;
+    return;
+  }
+
+  if (block.y < maxY) {
+    // Block kann weiter nach unten wandern
     block.y += blockSize;
   } else {
-    // Alter Block unten angekommen
-    blocks.push(block);
+    // block
+    // blocks
+    const filteredBlocks = blocks.filter((b) => b.x === block.x);
+    if (filteredBlocks.length > 0) {
+      const lastBlockInXAxis = filteredBlocks[filteredBlocks.length - 1];
+      if (lastBlockInXAxis.c === block.c) {
+        const index = blocks.indexOf(lastBlockInXAxis);
+        blocks.splice(index, 1);
+        // Alternative
+        // blocks = blocks.filter((b) => b !== lastBlockInXAxis);
+        points++;
+      } else {
+        blocks.push(block);
+      }
+    } else {
+      blocks.push(block);
+    }
+
     // Neuer Block kann an den Start
-    block = { y: 0, x: 0, c: random(colors) };
-    console.log(blocks);
+    const randomX = Math.floor(random(10)) * blockSize;
+    block = { x: randomX, y: 0, c: random(colors) };
   }
 
   for (let i = 0; i < blocks.length; i++) {
     fill(blocks[i].c);
     rect(blocks[i].x, blocks[i].y, blockSize, blockSize);
   }
+}
+
+function getMaxYforX(x) {
+  if (blocks.length > 0) {
+    const filteredBlocks = blocks.filter((b) => b.x === x);
+    if (filteredBlocks.length > 0) {
+      return filteredBlocks[filteredBlocks.length - 1].y - blockSize;
+    }
+  }
+
+  return height - blockSize;
 }
 
 function keyPressed(e) {
@@ -47,5 +92,12 @@ function keyPressed(e) {
     if (block.x > 0) {
       block.x -= blockSize;
     }
+  }
+}
+
+function mousePressed() {
+  if (gameover === true) {
+    blocks = [];
+    gameover = false;
   }
 }
