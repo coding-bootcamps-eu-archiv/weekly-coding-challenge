fea05d417f092d3097ce12e48a3a08de38367676
Nico <nico.koenig@coding-bootcamps.eu>
2022-03-10 19:02:39 +0100

    feat: adds final version for ttt

--- 2022-10/script.js
+++ 2022-10/script.js
@@ -1,32 +1,78 @@
 const ttt = document.querySelector(".ttt");
 
-let circle = true;
+let isCircle = true;
 let counter = 0;
+const circle = 1;
+const winCircle = 3 * circle;
+const cross = 9;
+const winCross = 3 * cross;
+
+const game = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+let winFields = null;
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// returns winner or -1
+function checkWin() {
+  for (let i = 0; i < winConditions.length; i++) {
+    const winCondition = winConditions[i];
+    const checkSum =
+      game[winCondition[0]] + game[winCondition[1]] + game[winCondition[2]];
+    if (checkSum === winCircle) {
+      winFields = winCondition;
+      return circle;
+    } else if (checkSum === winCross) {
+      winFields = winCondition;
+      return cross;
+    }
+  }
+  return 0;
+}
 
 ttt.addEventListener("click", function (e) {
   const field = e.target;
   if (field.classList.contains("ttt-field") && field.classList.length === 1) {
     counter++;
 
-    if (circle) {
+    // Array.prototype.indexOf.call($0.children, $0.children[0])
+
+    const index = field.getAttribute("data-index");
+
+    if (isCircle) {
       field.classList.add("ttt-field-circle");
+      game[index] = circle;
     } else {
       field.classList.add("ttt-field-cross");
+      game[index] = cross;
     }
-    circle = !circle;
+    isCircle = !isCircle;
 
-    // const winner = checkWinner()
-    // if(winner === "o") {
-    //     alert("hurra, o!")
-    // } else if(winner === "x") {
-    //     alert("hurra, x!")
-    // }
+    const winner = checkWin();
 
-    if (counter === 9) {
-      alert("ENDE");
+    if (winner !== 0) {
+      const winnerEl = document.querySelector("#winner");
+      if (winner === circle) {
+        winnerEl.innerText = "The Winner is Circle!";
+        winnerEl.removeAttribute("hidden");
+      } else if (winner === cross) {
+        winnerEl.innerText = "The Winner is Cross!";
+        winnerEl.hidden = false;
+      }
+
+      winFields.forEach((winField) => {
+        const field = ttt.children[winField];
+        field.classList.add("ttt-field-won");
+      });
     }
   }
 });
-
-// function checkWinner() {
-// }
