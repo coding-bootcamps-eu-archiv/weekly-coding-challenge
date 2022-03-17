daa466c8f1ba33bbf49a36f00701414f0fa4353e
Nico <nico.koenig@coding-bootcamps.eu>
2022-03-17 19:11:27 +0100

    feat: adds final version for memory

--- 2022-11/script.js
+++ 2022-11/script.js
@@ -1,45 +1,77 @@
 const main = document.querySelector("main");
 
-let counter = 0;
 let score = 0;
 let lastCard = null;
+let gameOn = true;
 
-main.addEventListener("click", function (e) {
+main.addEventListener("click", memory);
+
+function memory(e) {
   if (
     e.target.classList.contains("card") &&
-    e.target.classList.contains("card-matched") === false
+    e.target.classList.contains("card-matched") === false &&
+    gameOn === true
   ) {
+    console.log("adsfasdfadsf");
     const card = e.target;
     card.classList.add("card-visible");
-    counter++;
 
     if (lastCard === null) {
       lastCard = card;
     } else {
       if (lastCard.innerText === card.innerText) {
-        // Match
-
         card.classList.add("card-matched");
         lastCard.classList.add("card-matched");
-
         lastCard = null;
         score++;
         renderScore();
       } else {
-        // Kein Match
-
+        gameOn = false;
         setTimeout(function () {
           card.classList.remove("card-visible");
           lastCard.classList.remove("card-visible");
+          lastCard = null;
+          gameOn = true;
         }, 1000);
       }
     }
   }
-});
+}
 
 function renderScore() {
   const scoreEl = document.querySelector("#score");
   scoreEl.innerText = score;
 }
 
+function shuffle() {
+  const l = main.children.length;
+  let imageI = 0;
+  for (let i = 0; i < l; i = i + 2) {
+    console.log(i);
+    main.children[i].style.backgroundImage = "url(img/" + [imageI] + ".jpeg)";
+    main.children[i + 1].style.backgroundImage =
+      "url(img/" + [imageI] + ".jpeg)";
+
+    imageI++;
+  }
+
+  for (let i = 0; i < l; i++) {
+    main.insertBefore(
+      main.children[randomNumber(0, l - 1)],
+      main.children[randomNumber(0, l - 1)]
+    );
+  }
+
+  for (let i = 0; i < l; i++) {
+    main.children[i].id = i + 1;
+  }
+}
+
+shuffle();
+
 renderScore();
+
+function randomNumber(min, max) {
+  const num = Math.random() * (max - min + 1) + min;
+  return Math.floor(num);
+}
